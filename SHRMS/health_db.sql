41093a4ab435c84ec2451a49330fa6f898663364
Shubham Ghalsasi <ghalsasishubham@gmail.com>
2025-06-24 12:49:06 +0530

    Delete SHRMS/health_db.sql

--- SHRMS/health_db.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS health_db;
-USE health_db;
-
-CREATE TABLE IF NOT EXISTS patients (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100),
-    age INT,
-    diagnosis TEXT
-);
